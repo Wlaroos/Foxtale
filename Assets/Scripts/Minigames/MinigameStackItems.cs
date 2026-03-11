3187719960aeb8fb06fbfdb46855368f3a8d086b
Mikey Bess <mikeybess27@gmail.com>
2026-03-10 21:37:13 -0500

    Better stackable

--- Assets/Scripts/Minigames/MinigameStackItems.cs
+++ Assets/Scripts/Minigames/MinigameStackItems.cs
@@ -5,8 +5,10 @@ public class MinigameStackItems : BaseMinigame
     [SerializeField] private GameObject _stackablePrefab;
     [SerializeField] private GameObject _bottomAreaPrefab;
     [SerializeField] private int _items = 2;
+    [SerializeField] private GameObject _stackParticlePrefab;
     private GameObject[] _stackables;
     private GameObject _bottomArea;
+    private bool[] _stacked;
 
     protected override void StartMinigame()
     {
@@ -16,6 +18,8 @@ public class MinigameStackItems : BaseMinigame
             _stackables[i] = Instantiate(_stackablePrefab, GetRandomPositionInBounds(), Quaternion.identity, transform);
         }
 
+        _stacked = new bool[_items];
+
         _bottomArea = Instantiate(_bottomAreaPrefab, GetRandomPositionInBounds(), Quaternion.identity, transform);
     }
 
@@ -24,24 +28,47 @@ public class MinigameStackItems : BaseMinigame
         // Check if all stackables are close to the bottom area position
         Vector2 bottomAreaPosition = _bottomArea.transform.position;
 
-        bool[] stacked = new bool[_items];
+        if (_stackables == null || _bottomArea == null)
+            return;
 
         bool allStacked = true;
 
         for (int i = 0; i < _items; i++)
         {
+            if (_stackables[i] == null)
+            {
+                _stacked[i] = false;
+                allStacked = false;
+                continue;
+            }
+
             if (Vector2.Distance(_stackables[i].transform.position, bottomAreaPosition) < 0.5f)
             {
-                stacked[i] = true;
+                if (_stackParticlePrefab != null && _stacked[i] == false)
+                {
+                    Instantiate(_stackParticlePrefab, bottomAreaPosition, Quaternion.identity, transform);
+                }
+
+                _stacked[i] = true;
+
+                var sr = _stackables[i].GetComponent<SpriteRenderer>();
+                var script = _stackables[i].GetComponent<DraggableObject>();
+
+                Destroy(script); // Remove the draggable component to prevent further movement
+
+                if (sr != null) sr.color = new Color(0, 1, 0, 0.5f);
+
+                _stackables[i].transform.position = bottomAreaPosition; // Snap to bottom area position
             }
             else
             {
-                stacked[i] = false;
+                _stacked[i] = false;
+                allStacked = false;
             }
         }
 
 
-        foreach (bool isStacked in stacked)
+        foreach (bool isStacked in _stacked)
         {
             if (!isStacked)
             {
